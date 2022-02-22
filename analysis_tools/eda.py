08b6a611dfb14389a9df2bf491c51886e0fb7410
djy-git <djyoon0223@gmail.com>
2022-02-22 18:02:21 +0900

    [update] Parameter 위치 변경

--- analysis_tools/eda.py
+++ analysis_tools/eda.py
@@ -12,13 +12,16 @@ from sklearn.inspection import permutation_importance
 from sklearn.preprocessing import OrdinalEncoder
 
 
-def _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, suptitle):
+def _plot_on_ax(plot_fn, suptitle, ax, dir_path, figsize, show_plot):
     """Plot on a single axis if ax is not None. Otherwise, plot on a new figure.
 
     Parameters
     ----------
     plot_fn : function
         Function to plot.
+
+    suptitle : str
+        Title of the plot.
 
     ax : matplotlib.axes.Axes or list of matplotlib.axes.Axes
         Axis to plot.
@@ -31,9 +34,6 @@ def _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, suptitle):
 
     show_plot : bool
         Whether to show the plot.
-
-    suptitle : str
-        Title of the plot.
     """
     if ax is not None:
         plot_fn(ax)
@@ -77,76 +77,8 @@ def plot_missing_value(data, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT
         axes[1].set_xticklabels([])
 
 
-### Single feature
-def plot_num_feature(data_f, dir_path=None, bins=BINS, ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot histogram of a numeric feature.
-
-    Parameters
-    ----------
-    data_f : pandas.Series
-        Series to be analyzed.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    bins : int
-        Number of bins.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
-    >>> eda.plot_num_feature(data['a'], dir_path='.')
-    """
-    def plot_fn(ax):
-        sns.histplot(data_f, bins=bins, ax=ax, kde=True, stat='density')
-        ax.set_xlabel(None)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, data_f.name)
-def plot_cat_feature(data_f, dir_path=None,            ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot bar of a categorical feature.
-
-    Parameters
-    ----------
-    data_f : pandas.Series
-        Series to be analyzed.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
-    >>> eda.plot_cat_feature(data['b'], dir_path='.')
-    """
-    def plot_fn(ax):
-        density = data_f.value_counts(normalize=True).sort_index()
-        sns.barplot(density.index, density.values, ax=ax)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, data_f.name)
-
-
-### Multiple features
-def plot_features(data, dir_path=None, bins=BINS, n_cols=N_COLS, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+### Features
+def plot_features(data, bins=BINS, n_cols=N_COLS,                          dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
     """Plot histogram or bar for all features.
 
     Parameters
@@ -154,14 +86,14 @@ def plot_features(data, dir_path=None, bins=BINS, n_cols=N_COLS, figsize=FIGSIZE
     data : pandas.DataFrame
         DataFrame to be analyzed.
 
-    dir_path : str
-        Directory path to save the plot.
-
     bins : int
         Number of bins.
 
     n_cols : int
         Number of columns.
+
+    dir_path : str
+        Directory path to save the plot.
 
     figsize : tuple
         Figure size.
@@ -194,177 +126,7 @@ def plot_features(data, dir_path=None, bins=BINS, n_cols=N_COLS, figsize=FIGSIZE
                 cnts = data[f].value_counts(normalize=True).sort_index()  # normalize including NaN
                 ax.bar(cnts.index, cnts.values, width=0.5, alpha=0.5)
                 ax.set_xticks(cnts.index)
-def plot_num_num_features(data, f1, f2, dir_path=None, bins=BINS, ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot histogram of two numeric features.
-
-    Parameters
-    ----------
-    data : pandas.DataFrame
-        DataFrame to be analyzed.
-
-    f1 : str
-        First numerical feature.
-
-    f2 : str
-        Second numerical feature.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    bins : int
-        Number of bins.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
-    >>> eda.plot_num_num_features(data, 'a', 'c', dir_path='.')
-    """
-    def plot_fn(ax):
-        sns.histplot(x=data[f1], y=data[f2], bins=bins, ax=ax)
-        ax.set_xlabel(None);  ax.set_ylabel(None)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, f"{f1} vs {f2}")
-def plot_num_cat_features(data, f1, f2, dir_path=None, n_classes=N_CLASSES_PLOT, ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot violinplot of categorical, numerical features.
-
-    Parameters
-    ----------
-    data : pandas.DataFrame
-        DataFrame to be analyzed.
-
-    f1 : str
-        First numerical feature.
-
-    f2 : str
-        Second categorical feature.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    n_classes : int
-        Number of classes to plot.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
-    >>> eda.plot_num_cat_features(data, 'a', 'b', dir_path='.')
-    """
-    def plot_fn(ax):
-        selected_classes = data[f2].value_counts().index[:n_classes]
-        idxs_selected    = data[f2][data[f2].isin(selected_classes)].index
-        data_f1, data_f2 = data[f1][idxs_selected], data[f2][idxs_selected]
-        sns.violinplot(x=data_f1, y=data_f2, ax=ax, orient='h', order=reversed(sorted(selected_classes)), cut=0)
-        ax.set_xlabel(None);  ax.set_ylabel(None)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, f"{f1} vs {f2}")
-def plot_cat_num_features(data, f1, f2, dir_path=None, n_classes=N_CLASSES_PLOT, ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot violinplot of categorical, numerical features.
-
-    Parameters
-    ----------
-    data : pandas.DataFrame
-        DataFrame to be analyzed.
-
-    f1 : str
-        First categorical feature.
-
-    f2 : str
-        Second numerical feature.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    n_classes : int
-        Number of classes to plot.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
-    >>> eda.plot_cat_num_features(data, 'b', 'a', dir_path='.')
-    """
-    def plot_fn(ax):
-        selected_classes = data[f1].value_counts().index[:n_classes]
-        idxs_selected    = data[f1][data[f1].isin(selected_classes)].index
-        data_f1, data_f2 = data[f1][idxs_selected], data[f2][idxs_selected]
-        sns.violinplot(x=data_f1, y=data_f2, ax=ax, orient='v', order=sorted(selected_classes), cut=0)
-        ax.set_xlabel(None);  ax.set_ylabel(None)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, f"{f1} vs {f2}")
-def plot_cat_cat_features(data, f1, f2, dir_path=None, n_classes=N_CLASSES_PLOT, ax=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
-    """Plot heatmap of two categorical features.
-
-    Parameters
-    ----------
-    data : pandas.DataFrame
-        DataFrame to be analyzed.
-
-    f1 : str
-        First categorical feature.
-
-    f2 : str
-        Second categorical feature.
-
-    dir_path : str
-        Directory path to save the plot.
-
-    n_classes : int
-        Number of classes to plot.
-
-    ax : matplotlib.axes.Axes
-        Axis to plot.
-
-    figsize : tuple
-        Figure size.
-
-    show_plot : bool
-        Whether to show the plot.
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import analysis_tools.eda as eda
-    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': ['a10', 'b22', 'c11', 'a10', 'b22']})
-    >>> eda.plot_cat_cat_features(data, 'b', 'c', dir_path='.')
-    """
-    def plot_fn(ax):
-        ratio = pd.crosstab(data[f2], data[f1], normalize='columns')
-        ratio.sort_index(inplace=True, ascending=False)  # sort by index
-        ratio = ratio[sorted(ratio)]                     # sort by column
-        ratio = ratio.iloc[:n_classes, :n_classes]
-        sns.heatmap(ratio, ax=ax, annot=True, fmt=".2f", cmap=sns.light_palette('firebrick', as_cmap=True), cbar=False)
-        ax.set_xlabel(None);  ax.set_ylabel(None)
-    _plot_on_ax(plot_fn, ax, dir_path, figsize, show_plot, f"{f1} vs {f2}")
-def plot_features_target(data, target, dir_path=None, n_cols=N_COLS, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+def plot_features_target(data,  target, n_cols=N_COLS,                     dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
     """Plot features vs target.
 
     Parameters
@@ -410,7 +172,7 @@ def plot_features_target(data, target, dir_path=None, n_cols=N_COLS, figsize=FIG
             ax.set_title(f"{f} vs {target}")
             f_type = 'num' if f in num_features else 'cat'
             eval(f"plot_{f_type}_{target_type}_features")(data, f, target, ax=ax)
-def plot_corr(corr, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+def plot_corr(corr,                                                        dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
     """Plot correlation matrix.
 
     Parameters
@@ -439,10 +201,245 @@ def plot_corr(corr, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
         mask = np.zeros_like(corr, dtype=np.bool)
         mask[np.triu_indices_from(mask)] = True
         sns.heatmap(corr, mask=mask, ax=ax, annot=True, fmt=".2f", cmap='coolwarm', center=0)
+def plot_num_feature(data_f, bins=BINS,                           ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot histogram of a numeric feature.
+
+    Parameters
+    ----------
+    data_f : pandas.Series
+        Series to be analyzed.
+
+    bins : int
+        Number of bins.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
+    >>> eda.plot_num_feature(data['a'], dir_path='.')
+    """
+    def plot_fn(ax):
+        sns.histplot(data_f, bins=bins, ax=ax, kde=True, stat='density')
+        ax.set_xlabel(None)
+    _plot_on_ax(plot_fn, data_f.name, ax, dir_path, figsize, show_plot)
+def plot_cat_feature(data_f,                                      ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot bar of a categorical feature.
+
+    Parameters
+    ----------
+    data_f : pandas.Series
+        Series to be analyzed.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
+    >>> eda.plot_cat_feature(data['b'], dir_path='.')
+    """
+    def plot_fn(ax):
+        density = data_f.value_counts(normalize=True).sort_index()
+        sns.barplot(density.index, density.values, ax=ax)
+    _plot_on_ax(plot_fn, data_f.name, ax, dir_path, figsize, show_plot)
+def plot_num_num_features(data, f1, f2, bins=BINS,                ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot histogram of two numeric features.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to be analyzed.
+
+    f1 : str
+        First numerical feature.
+
+    f2 : str
+        Second numerical feature.
+
+    bins : int
+        Number of bins.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
+    >>> eda.plot_num_num_features(data, 'a', 'c', dir_path='.')
+    """
+    def plot_fn(ax):
+        sns.histplot(x=data[f1], y=data[f2], bins=bins, ax=ax)
+        ax.set_xlabel(None);  ax.set_ylabel(None)
+    _plot_on_ax(plot_fn, f"{f1} vs {f2}", ax, dir_path, figsize, show_plot)
+def plot_num_cat_features(data, f1, f2, n_classes=N_CLASSES_PLOT, ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot violinplot of categorical, numerical features.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to be analyzed.
+
+    f1 : str
+        First numerical feature.
+
+    f2 : str
+        Second categorical feature.
+
+    n_classes : int
+        Number of classes to plot.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
+    >>> eda.plot_num_cat_features(data, 'a', 'b', dir_path='.')
+    """
+    def plot_fn(ax):
+        selected_classes = data[f2].value_counts().index[:n_classes]
+        idxs_selected    = data[f2][data[f2].isin(selected_classes)].index
+        data_f1, data_f2 = data[f1][idxs_selected], data[f2][idxs_selected]
+        sns.violinplot(x=data_f1, y=data_f2, ax=ax, orient='h', order=reversed(sorted(selected_classes)), cut=0)
+        ax.set_xlabel(None);  ax.set_ylabel(None)
+    _plot_on_ax(plot_fn, f"{f1} vs {f2}", ax, dir_path, figsize, show_plot)
+def plot_cat_num_features(data, f1, f2, n_classes=N_CLASSES_PLOT, ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot violinplot of categorical, numerical features.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to be analyzed.
+
+    f1 : str
+        First categorical feature.
+
+    f2 : str
+        Second numerical feature.
+
+    n_classes : int
+        Number of classes to plot.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': [1.2, 2.3, 3.4, 4.5, 5.6]})
+    >>> eda.plot_cat_num_features(data, 'b', 'a', dir_path='.')
+    """
+    def plot_fn(ax):
+        selected_classes = data[f1].value_counts().index[:n_classes]
+        idxs_selected    = data[f1][data[f1].isin(selected_classes)].index
+        data_f1, data_f2 = data[f1][idxs_selected], data[f2][idxs_selected]
+        sns.violinplot(x=data_f1, y=data_f2, ax=ax, orient='v', order=sorted(selected_classes), cut=0)
+        ax.set_xlabel(None);  ax.set_ylabel(None)
+    _plot_on_ax(plot_fn, f"{f1} vs {f2}", ax, dir_path, figsize, show_plot)
+def plot_cat_cat_features(data, f1, f2, n_classes=N_CLASSES_PLOT, ax=None, dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+    """Plot heatmap of two categorical features.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to be analyzed.
+
+    f1 : str
+        First categorical feature.
+
+    f2 : str
+        Second categorical feature.
+
+    n_classes : int
+        Number of classes to plot.
+
+    ax : matplotlib.axes.Axes
+        Axis to plot.
+
+    dir_path : str
+        Directory path to save the plot.
+
+    figsize : tuple
+        Figure size.
+
+    show_plot : bool
+        Whether to show the plot.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import analysis_tools.eda as eda
+    >>> data = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': ['a', 'b', 'c', 'd', 'e'], 'c': ['a10', 'b22', 'c11', 'a10', 'b22']})
+    >>> eda.plot_cat_cat_features(data, 'b', 'c', dir_path='.')
+    """
+    def plot_fn(ax):
+        ratio = pd.crosstab(data[f2], data[f1], normalize='columns')
+        ratio.sort_index(inplace=True, ascending=False)  # sort by index
+        ratio = ratio[sorted(ratio)]                     # sort by column
+        ratio = ratio.iloc[:n_classes, :n_classes]
+        sns.heatmap(ratio, ax=ax, annot=True, fmt=".2f", cmap=sns.light_palette('firebrick', as_cmap=True), cbar=False)
+        ax.set_xlabel(None);  ax.set_ylabel(None)
+    _plot_on_ax(plot_fn, f"{f1} vs {f2}", ax, dir_path, figsize, show_plot)
 
 
 ### Feature importance
-def get_feature_importance(data, target, dir_path=None, problem='classification', bins=BINS, figsize=FIGSIZE, show_plot=SHOW_PLOT):
+def get_feature_importance(data, target, bins=BINS, problem='classification', dir_path=None, figsize=FIGSIZE, show_plot=SHOW_PLOT):
     """Get feature importance using RandomForest model.
 
     The metrics are mean decrease in impurity, mean accuracy decrease, mean rank
@@ -455,14 +452,14 @@ def get_feature_importance(data, target, dir_path=None, problem='classification'
     target : str
         Target feature.
 
-    dir_path : str
-        Directory path to save the plot.
+    bins : int
+        Number of bins.
 
     problem : str
         Problem type.(`classification` or `regression`)
 
-    bins : int
-        Number of bins.
+    dir_path : str
+        Directory path to save the plot.
 
     figsize : tuple
         Figure size.
