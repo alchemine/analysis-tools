15e50838eb6b81c0c4a0d5467782f3a573393ce6
djy-git <djyoon0223@gmail.com>
2022-03-01 12:24:49 +0900

    [update] 1. Remame: save_tree() -> save_tree_visualization() 2. Add parameter: sample_test

--- analysis_tools/modeling.py
+++ analysis_tools/modeling.py
@@ -23,7 +23,7 @@ def get_scaling_model(model, scaler=StandardScaler()):
     return make_pipeline(scaler, model)
 
 
-def save_tree(fitted_model, X, y, file_path, feature_names=None, class_names=None):
+def save_tree_visualization(fitted_model, X, y, file_path, feature_names=None, class_names=None, orientation='LR', test_sample=None):
     """
     Save a dtreeviz visualization of the given model.
 
@@ -33,19 +33,26 @@ def save_tree(fitted_model, X, y, file_path, feature_names=None, class_names=Non
         sklearn model fitted.
 
     X : pandas.dataframe or numpy.array
-        feature array
+        Feature array
 
     y : pandas.series or numpy.array
-        target array
+        Target array
 
     file_path : string
-        path to save the dtreeviz visualization. file_path must end with '.svg'.
+        Path to save the dtreeviz visualization. file_path must end with '.svg'.
 
     feature_names : list of strings
-        list of feature names.
+        List of feature names.
 
     class_names : list of strings
-        list of class names.
+        List of class names.
+
+    orientation : string
+        Orientation of the tree.
+        'LR' for left to right, 'TB' for top to bottom.
+
+    test_sample : pandas.series or numpy.array
+        One sample of test data
 
     Examples
     --------
@@ -59,8 +66,8 @@ def save_tree(fitted_model, X, y, file_path, feature_names=None, class_names=Non
     >>> model = DecisionTreeClassifier(max_depth=3)
     >>> model.fit(X, y)
 
-    >>> save_tree(model, X, y, 'iris_tree.svg', feature_names=iris.feature_names, class_names=list(iris.target_names))
+    >>> save_tree_visualization(model, X, y, 'iris_tree.svg', feature_names=iris.feature_names, class_names=list(iris.target_names), test_sample=X[0])
     """
-    viz = dtreeviz(fitted_model, X, y, feature_names=feature_names, class_names=class_names)
+    viz = dtreeviz(fitted_model, X, y, feature_names=feature_names, class_names=class_names, orientation=orientation, X=test_sample)
     assert file_path.endswith('.svg'), 'file_path must end with .svg'
     viz.save(file_path)
