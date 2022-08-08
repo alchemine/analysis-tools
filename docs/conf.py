7e861f8cc5cb426bbed3036223cf53b49bd88b53
djy-git <djyoon0223@gmail.com>
2022-08-09 02:50:14 +0900

    [update] Version: 0.1.1 -> 0.1.2 1. 사용하지 않는 `axes`에 대하여 `ax.axis('off')` 적용 2. `eda.plot_corr()`의 `mask`, `annot` argument 등 외부 접근성 증대 3. `common/config.py`의 전역변수를 `PARAMS`, `PLOT_PARAMS`로 패키징 4. `preprocessing.py` module 추가 5. 가급적 함수 안에서 external module을 로드하도록 수정 6. 예제 보강

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Dongjin Yoon'
 author = 'Dongjin Yoon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
