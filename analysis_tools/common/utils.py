cf22085163d139fa4e4ec6078dbfd79f972809ce
djy-git <djyoon0223@gmail.com>
2022-08-20 15:43:30 +0900

    [update] Remove `name()`

--- analysis_tools/common/utils.py
+++ analysis_tools/common/utils.py
@@ -194,20 +194,3 @@ class SeabornFig2Grid:
 
     def _resize(self, evt=None):
         self.sg.fig.set_size_inches(self.fig.get_size_inches())
-
-
-def name(var):
-    # https://stackoverflow.com/a/18425523
-    """Get name of variable
-
-    Parameters
-    ----------
-    var : anything
-        Variable
-
-    Returns
-    -------
-    Name of variable
-    """
-    callers_local_vars = inspect.currentframe().f_back.f_locals.items()
-    return [var_name for var_name, var_val in callers_local_vars if var_val is var][0]
