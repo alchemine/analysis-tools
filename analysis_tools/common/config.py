f8ee755d8697538478c06835225461dd588024d2
djy-git <djyoon0223@gmail.com>
2022-08-20 15:13:03 +0900

    [update] common/config.py | Update `PARAMS`, `PLOT_PARAMS` and add comment

--- analysis_tools/common/config.py
+++ analysis_tools/common/config.py
@@ -8,33 +8,75 @@ Commonly used constant parameters are defined in capital letters.
 
 # Common parameters
 class PARAMS:
+    """Global parameters
+
+    Parameters
+    ----------
+    seed : int
+        Random seed
+
+    test_size : float
+        Size of test set size when splitting dataset
+    """
     seed      = 42
     test_size = 0.2
 
     @classmethod
     def get(cls, key, val):
-        """
-        >>> PLOT_PARAMS.get('figsize', (20, 10))
-        >>> PLOT_PARAMS.get('alpha', {'alpha': 0.5})
+        """Return member variable(key) if val is None else val
+
+        Parameters
+        ----------
+        key : str
+            Name of parameter
+
+        val : any dtype
+            Value of the parameter
+
+        >>> assert PARAMS.get('seed', 1) == 1
+        >>> assert PARAMS.get('seed', None) == PARAMS.seed
+        >>> assert PLOT_PARAMS.get('figsize', (20, 10)) == (20, 10)
+        >>> assert PLOT_PARAMS.get('alpha', {'alpha': 0.5}) == 0.5
         """
         if val is None:
             return getattr(cls, key)
         else:
             if isinstance(val, dict):
-                if key in val:
-                    return val[key]
-                else:
-                    return getattr(cls, key)
+                return val[key] if key in val else getattr(cls, key)
             return val
 
 
 # Plot parameters
 class PLOT_PARAMS(PARAMS):
-    show_plot      = True
-    figsize        = (30, 10)
-    bins           = 50
-    n_classes      = 5
-    n_cols         = 5
-    n_subsets_step = 5
-    alpha          = 0.3
-    s              = 20  # size of point in scatterplot
+    """Global plot parameters
+
+    Parameters
+    ----------
+    show_plot : bool
+        Whether to show plot
+
+    figsize : tuple
+        Figure size
+
+    bins : int
+        Number of bins for histogram plot
+
+    n_classes : int
+        Number of classes to show for categorical features
+
+    n_cols : int
+        Number of columns for subplots figure
+
+    alpha : float
+        Transparency
+
+    marker_size : float
+        Marker size for scatter plot
+    """
+    show_plot   = True
+    figsize     = (30, 10)
+    bins        = 50
+    n_classes   = 5
+    n_cols      = 5
+    alpha       = 0.3
+    marker_size = 20
