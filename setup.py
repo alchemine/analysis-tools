6f01a36b1aaaa2f5ca8a6d36eac2e86123356469
djy-git <djyoon0223@gmail.com>
2022-11-09 03:54:45 +0900

    [update] Version: 0.1.2 -> 0.2.1 1. `eda` module - `plot_ts_features()` 추가 - `plot_pair()` 추가 - `plot_features()`, `plot_corr()`: 2개의 data를 비교할 수 있도록 변경
    2. `common/utils` module
    - `lmap()` 추가
    - `lstarmap()` 추가
    - `ini2dict()` 추가
    - `str2dt()`, `dt2str()` 추가
    - `double2float()` 추가
    - `farray()`, `iarray()` 추가
    - `COLORS` 추가
    - `vars_()` 추가
    
    기타 error fix, refactoring

--- setup.py
+++ setup.py
@@ -14,16 +14,30 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="analysis-tools",
-    version="0.1.2",
+    version="0.2.1",
     author="Dongjin Yoon",
     author_email="djyoon0223@gmail.com",
     description="Analysis tools for machine learning projects",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/djy-git/analysis-tools",
-    packages=setuptools.find_packages(),
+    install_requires=[
+        'dask[complete]==2022.10.2',
+        'dtreeviz==1.4.0',
+        'joblib==1.2.0',
+        'matplotlib==3.6.2',
+        'missingno==0.5.1',
+        'numba==0.56.4',
+        'numpy==1.23.4',
+        'pandas==1.5.1',
+        'scikit-learn==1.1.3',
+        'seaborn==0.12.1',
+        'switch==1.1.0',
+        'tabulate==0.9.0',
+        'tqdm==4.64.1',
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
 )
